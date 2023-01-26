869234cb7adb15ada4f1f070a4d9a2a0efe6bcb8
Cine-Pixel <61045277+Cine-Pixel@users.noreply.github.com>
2023-01-26 19:28:31 +0400

    feat(): Custom authentication (#9)
    * feat(): users app init
    
    * feat(): custom user authentication

---
+++ ecommerce/users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CommerceUser
+
+
+class CommerceUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CommerceUser
+        fields = ("first_name", "last_name", "email", "password1", "password2", )
+
+
+class CommerceUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CommerceUser
+        fields = ("email",)
