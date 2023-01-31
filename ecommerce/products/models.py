9190e3d92a01a626a6a318ccf7205b091dc38f35
BMGomes22 <116948446+BMGomes22@users.noreply.github.com>
2023-01-31 19:37:24 +0400

    Product Category

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=700)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField()
-    category = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -18,3 +28,5 @@ class Image(models.Model):
 
     def __str__(self):
         return self.image.path
+
+
