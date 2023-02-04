86553725459fd494f7da5c61578798c6e2b23a67
Cine-Pixel <61045277+Cine-Pixel@users.noreply.github.com>
2023-02-04 09:53:14 +0400

    feat(): upload product with image in admin panel (#26)

--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Image, Product, Category
 
+class ImageInline(admin.TabularInline):
+    model = Image
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Image)
 admin.site.register(Category)
